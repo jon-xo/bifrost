e86372803e3f25b28bc3657a33e123ca71ea95f7
Jon Newton <jonn@hey.com>
2021-06-20 09:38:26 -0400

    Updated App.js with Bulma css import. Created Header.js with b asic styled navigation. Added Header.js to ApplicationView.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ApplicationViews from "./components/ApplicationViews";
 import { ComicProvider } from './providers/ComicProvider';
 import logo from './logo.svg';
+import 'bulma/css/bulma.min.css';
 import './App.css';
 
 function App() {
   return (
       <Router>
         <ComicProvider>
-          <ApplicationViews />
+          <div className="main">
+            <ApplicationViews />
+          </div>
         </ComicProvider>
       </Router>
   );
--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { ComicContext } from "../providers/ComicProvider";
+import Header from "./Header"
 
 export default function ApplicationViews() {
     return (
         <main>
             <Switch>
                 <Route exact path="/">
+                    <Header />
                 </Route>
             </Switch>
         </main>

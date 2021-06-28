c60a232c5ecdd4151f75b813e4fb363560c86e1e
Jon Newton <jonn@hey.com>
2021-06-28 10:57:27 -0400

    Updated SQL scripts to include DisplayName value in table and test user. Added Utils directory with DbUtils.cs file, created Models directory with UserAccount.cs file.

--- SQL/02_Bifrost_BuildData.sql
+++ SQL/02_Bifrost_BuildData.sql
@@ -2,5 +2,5 @@
 GO
 
 set identity_insert [UserAccount] on
-    insert into [UserAccount] (Id, Name, Email, Firebase_Id, UserSummary, CreateDateTime, ImageLocation, Private) values (1, 'Test Admin', 'admin@example.com', '4tU6x2KaBJfPjn7GoS4siYMiPl82', 'Even test users love comics!', '2021-06-25', null, 0);
+    insert into [UserAccount] (Id, [Name], DisplayName, Email, Firebase_Id, UserSummary, CreateDateTime, ImageLocation, [Private]) values (1, 'Test Admin', 'rougeOneX' , 'admin@example.com', '4tU6x2KaBJfPjn7GoS4siYMiPl82', 'Even test users love comics!', '2021-06-25', null, 0);
 set identity_insert [UserAccount] off
---
+++ bifrost/bifrost/Models/UserAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace bifrost.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        [MaxLength(255)]
+        [DataType(DataType.Url)]
+        public string ImageLocation { get; set; }
+
+        [MaxLength(280)]
+        public string UserSummary { get; set; }
+
+        [Required]
+        public Boolean Private { get; set; }
+    }
+}
